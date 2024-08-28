97cb89dea415e46f1e3066b3d466d74814dd18bc
finteger <nashassociatesinc@gmail.com>
2024-08-28 12:15:10 -0600

    Update

--- app.js
+++ app.js
@@ -4,20 +4,16 @@ const app = express();
 const YAML = require('yamljs');
 const swaggerUI = require('swagger-ui-express');
 const bodyParser = require('body-parser');
-require('dotenv').config();
 const authRoutes = require('./routes/authRoutes.js');
 const studentRoutes = require('./routes/studentRoutes.js');
 const PORT = process.env.PORT || 3000;
-
+const initializeDatabase = require('./data/initializeDatabase');
+require('dotenv').config();
 
 //Swagger Documentation
 const swaggerDocument = YAML.load('./swagger.yaml');
 app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
-//Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI)
-.then(() => { console.log('Connected to MongoDB Database') })
-.catch((err) => { console.log(`Error connecting to database: ${err}`) });
 
 //View engines
 app.set('view engine', 'ejs');
@@ -29,17 +25,25 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/', authRoutes);
 app.use('/', studentRoutes);
-
-
 app.use((err, res, req, next) =>{
     console.error(err.stack);
     res.status(500).send('Internal Server Error');
     next();
 });
 
+//Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+.then(async () => { 
+    console.log('Connected to MongoDB Database');
+
+        await initializeDatabase();
+
+        //Start server
+        app.listen(PORT, () =>{
+            console.log(`Connected to port ${PORT}`);
+        });
+   
+})
+.catch((err) => { console.log(`Error connecting to database: ${err}`) });
 
 
-//Start server
-app.listen(PORT, () =>{
-    console.log(`Connected to port ${PORT}`);
-});
--- data/initializeDatabase.js
+++ data/initializeDatabase.js
@@ -1,11 +1,15 @@
 const fs = require('fs');
 const mongoose = require('mongoose');
-const Student = require('../models/studentRecord');
+const Student = require('../models/studentRecord.js');
 
 //load student data from JSON file
 async function loadStudentData(){
     return new Promise((resolve, reject) => {
     fs.readFile('students.json', 'utf-8', (err, data) =>{
+        if(err){
+          return reject(err);
+        }
+
         resolve(JSON.parse(data));
     });
   });
@@ -15,6 +19,8 @@ async function loadStudentData(){
 async function initializeDatabase(){
     try{
         const students = await loadStudentData();
+
+     
 
 
         for(const student in students){
@@ -29,9 +35,10 @@ async function initializeDatabase(){
           }
 
         }
-    }catch(error){
-  
+        console.log('Database initialization complete.')
+    }catch(err){
+      console.error('Error initializing database', err);
     }
-}
+  };
 
-module.exports = initializeDatabase;
+  module.exports = initializeDatabase;
